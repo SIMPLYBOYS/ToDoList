99bf54bd76b60077bf2b304980cde5b1856fe9f8
Aaron Chou <aaron@ecrowdmedia.com>
2014-03-16 23:26:48 +0800

    Use r.js to optimal the require.js project

---
+++ public/build.js
@@ -0,0 +1,8 @@
+({
+   mainConfigFile: 'javascripts/main.js',
+   baseUrl: 'javascripts',
+   name: 'main',
+   out: 'javascripts/main-built.js',
+   removeCombined: true,
+   findNestedDependencies: true
+})
